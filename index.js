d6dbfa546684d0b093ec8af21b5c8055a837f757
Kosty20 <104622649+Kosty20@users.noreply.github.com>
2023-06-06 17:17:47 +0300

    Finished start menu and PvP mode

--- index.js
+++ index.js
@@ -1,51 +1,148 @@
-window.addEventListener('click', startGame, { once: true });
-const btns = document.querySelector('[data-btns]');
+document.addEventListener('DOMContentLoaded', startGame);
+
+let turnX = true;
+let mode = '';
+let backIndex = 0;
 
 function startGame() {
+	eventHandlers();
+}
+
+function eventHandlers() {
+	const startMenu = document.querySelector('#startMenu');
+	const playArea = document.querySelector('#playArea');
+
+	//startMenu
 	const p = document.querySelector('[data-p]');
-    p.classList.add('shrink');
+	const back = document.querySelector('[data-back]');
+	const btn0 = document.querySelector('[data-pvp-btn]');
+	const btn1 = document.querySelector('[data-pvc-btn]');
+	const btn2 = document.querySelector('[data-easy-btn]');
+	const btn3 = document.querySelector('[data-hard-btn]');
+	const btn4 = document.querySelector('[data-impos-btn]');
 
-	const vsP = buildBtn('vs Player');
-	const vsC = buildBtn('vs Computer');
+	window.addEventListener(
+		'click',
+		() => {
+			toggleHidden(p);
+			toggleModeBtns();
+		},
+		{ once: true }
+	);
 
-	vsP.addEventListener('click', clearWindow)
+	back.addEventListener('click', () => {
+		if (backIndex === 1) {
+			toggleDifficultyBtns();
+			toggleModeBtns();
+			backIndex--;
+			toggleHidden(back);
+		}
+	});
 
-	vsC.addEventListener('click', () => {
-		vsP.remove();
-		vsC.remove();
+	btn1.addEventListener('click', () => {
+		toggleHidden(back);
+		toggleModeBtns();
+		toggleDifficultyBtns();
+		backIndex++;
+	});
 
-		const easy = buildBtn('Easy');
-		const medium = buildBtn('Normal');
-		const hard = buildBtn('Hard');
+	btn0.onclick = () => (mode = 'PvP');
+	btn2.onclick = () => (mode = 'PvC-Easy');
+	btn3.onclick = () => (mode = 'PvC-Hard');
+	btn4.onclick = () => (mode = 'PvC-Impos');
 
-		btns.append(easy, medium, hard);
-	})
+	const btns = [btn0, btn2, btn3, btn4];
+	btns.forEach((btn) => {
+		btn.addEventListener('click', () => {
+			startMenu.classList.add('hidden');
+			playArea.classList.remove('hidden');
+		});
+	});
 
-	btns.append(vsP, vsC);
-}
+	//playArea
+	const board = document.querySelector('[data-board]');
+	[...board.children].forEach((div) => {
+		div.addEventListener('click', () => manageModes(div), { once: true });
+	});
 
-function clearWindow (){
-	[...document.body.children].forEach(elmt => {
-		elmt.remove();
-	})
-}
-
-function buildBoard () {
-	const container = document.createElement('div');
-	container.classList.add('container');
-
-	for(let i = 1; i <= 9; i++){
-		const div = document.createElement('div');
-
-		
-
-		container.append(div);
+	//auxiliaries
+	function toggleHidden(target) {
+		target.classList.toggle('hidden');
+	}
+	function toggleModeBtns() {
+		[btn0, btn1].forEach(toggleHidden);
+	}
+	function toggleDifficultyBtns() {
+		[btn2, btn3, btn4].forEach(toggleHidden);
 	}
 }
 
-function buildBtn(text, cls) {
-	const btn = document.createElement('button');
-	btn.className = cls || '';
-	btn.textContent = text;
-	return btn;
+function manageModes(target) {
+	switch (mode) {
+		case 'PvP':
+			enablePvP(target);
+			break;
+		
+	}
+}
+
+function enablePvP(target) {
+	if (turnX) {
+		buildX(target);
+		target.classList.add('X');
+		turnX = false;
+	} else {
+		buildO(target);
+		target.classList.add('O');
+		turnX = true;
+	}
+	checkWinner();
+}
+
+function checkWinner() {
+	const divs = document.querySelectorAll('.board > div');
+
+	if (matched()) {
+		console.log('winner');
+	}
+
+	function matched() {
+		if (
+			checkDivs(0, 1, 2) ||
+			checkDivs(3, 4, 5) ||
+			checkDivs(6, 7, 8) ||
+			checkDivs(0, 3, 6) ||
+			checkDivs(1, 4, 7) ||
+			checkDivs(2, 5, 8) ||
+			checkDivs(0, 4, 8) ||
+			checkDivs(2, 4, 6)
+		) {
+			return true;
+		}
+	}
+
+	function checkDivs(a, b, c) {
+		if (
+			divs[a].className &&
+			divs[a].className === divs[b].className &&
+			divs[b].className === divs[c].className
+		) {
+			return true;
+		}
+	}
+}
+
+function buildX(target) {
+	const div1 = document.createElement('div');
+	const div2 = document.createElement('div');
+	div1.classList.add('x-line');
+	div2.classList.add('x-line');
+
+	target.append(div1, div2);
+}
+
+function buildO(target) {
+	const div = document.createElement('div');
+	div.classList.add('o-div');
+	target.append(div);
 }
